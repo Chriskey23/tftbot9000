b3d5566603a235d9928ea0c91df8f7e828ae6201
STKalinowski <kalinowski.stas@gmail.com>
2021-10-20 13:24:23 -0700

    Just added a bunch of things

--- controller.py
+++ controller.py
@@ -3,55 +3,45 @@
 
 import pyautogui
 import pydirectinput
-'''
-Letssay the game wants to move items from x to y
-to use item x on slot y
-drag(posX, posY)
-We could just be given item and 
-Should we just make it
+import cv2 as cv
+import os
 
-I guess we could give all the controls even though
-our bot wouldn't be using it all but maybe in the future?
-But how deep should they be?
-Should we just have it input by position or by a unit type?
-I think just position should be fine and have the bot handle 
-the whole thing?
-
-!!!Resolution of game window + resolution of monitor/display
-Will
-'''
-
-###!!!Resolution fo game window + resolution of montior/display
-###Effect where things will be. Will implement hardcoded for now
-#Potential solution is identifying common elements and getting
-#the rest of the positions later.
 '''
 def initAndCheckProperSettings():
     #Checks that the game is in proper resolution
     #and that the coloring is correct
 '''
 
-
 def buyExp():
     pydirectinput.press('f')
+    #Update exp and level
 
 def rerollShop():
     pydirectinput.press('d')
+    #Get shop info
 
 #Input everything to keep it funcitonal
 #Should I update and return the states too?
-def buyXPosition(pos,shopCoordinates, shopState, benchState, boardState, currentGold):
+def buyXPosition(pos, gameState):
+    #check that we can afford it, and then update bench as well.
+    #Update shop position
     pydirectinput.moveTo(pos)
     pydirectinput.dragTo(pos[0]+15, pos[1]+15, 0.12, button="left")
 
+    #Confirm that this occured?
+    #IE Screenshot and identify champ?
+
+def pickXArmoryPosition(pos, gameState):
+    pydirectinput.moveTo(pos)
+    pydirectinput.dragTo(pos[0]+15, pos[1]+15, 0.12, button="left")
+    #Update Bench
 
 
 
 def dragXPosToYPos(xCord, yCord, boardState, benchState):
     pydirectinput.moveTo(xCord[0], xCord[1])
-    pyautogui.dragTo(yCord[0], yCord[1],0.12,pyautogui.easeInQuad, button="left", )
+    pyautogui.dragTo(yCord[0], yCord[1],0.12 ,button="left")#,pyautogui.easeInQuad, button="left", )
     
-
 def sellXPos(xCord, boardState, benchState, currentGold):
     pyautogui.moveTo(xCord[0], xCord[1])
     pydirectinput.press('e')
@@ -60,36 +50,85 @@ def toggleHealthBars():
     pydirectinput.press('c')
 
 
-'''
-def getGameState():
-    #Going to assume healthbars are on, will add a check funciton in the future.
-    imHealthBars = pyautogui.screenshot(region=())
-    pyautogui.press('c')
-    imNoHealthBars = pyautogui.screenshot(region=())
-    pyautogui.press('c')
-
-    #Will need to check things like carosel, armour, stage
-
-    boardChampions,benchChampions = getBoardState(imNoHealthBars)
-    shopState = getBenchState(imHealthBars)
-
-    #Returns itemBench as well as updating items for all units with items
-    itemBench, boardChampions = getItemState(itemBench, boardChampions, )
-
-    playerState = getPlayerState()
+class champObject():
+    def __init__(self, name, items, starpower):
+        self.name = name
+        self.items = items
+        self.starpower = starpower
 
 
+class gameState():
+    def __init__(self):
+        self.board = ([None]*7)*4
+        self.bench = [None] * 9
+        self.hp = 100
+        #self.opponentHp
+        self.gold = 0
+        self.itemBench = [None]*10
+        self.stage = (0,0)
+        self.level = 1
+        self.stateOfStage = "carosel"
+        self.setupFlag
+        self.playerPosition 
+    
 
-    return (boardChampions,benchChampions,itemBench,shopState,playerState,gameStage)
-'''
-#We want to try and make it functional!
-#Can I evn make the control funcitons funcitonal?
-#I guess we could just have it carry out the operations
-#Whether they complete or not.
-#How costly is get gamestate going to be?
-#If it refreshes after each action
-#Then it could be expensive.
-#What we can do is only update where required.
-#So refreshing shop just checks that region.
-#So we begin round with stage, then after movement
-#We change, and then there is a verify if needed.
+
+class identifyCollection():
+    def __init__(self):
+        self.champTempalteImages = getImageList("./ImageTemplates/champTemplates/")
+        self.itemTempalteImages = getImageList("./ImageTemplates/itemTempaltes/")
+        #self.pickUpImages = getImageList("./ImageTempaltes/")
+        self.shopImages = getImageList("./ImageTempaltes/shopImages/")
+
+        self.planWarnColor
+        self.fightColor
+
+def getImageList(directory):
+    #Directory of images
+    #Go through and open each image + convert to color scheme
+    retVal = []
+    print("helo")
+
+
+#Goes through each element of imgPair and applies template matching to imgToCheck
+#If bellow threshhold, then name appended to the list 
+#imgPairList is a list of (name, tempalteImg)
+def templateMatchList(imgToCheck, imgPairList, threshHold):
+    method = cv.TM_SQDIFF_NORMED
+    listOfMatchNames = []
+    listOfPositions = []
+
+    for i in  imgPairList:
+        res = cv.matchTamplate(imgToCheck, i[1], method)
+        minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(res)
+        if minVal <= threshHold:
+            listOfMatchNames.append(i[0])
+            listOfPositions.append(minLoc)
+
+    return listOfMatchNames, listOfPositions
+
+def identifyChampion(champImages,starImages, itemImages, gameImage):
+    #First we need to identify the champion
+    threshHold = 1
+    rightSide = gameImage.crop()
+    champ,_ = templateMatchList(rightSide, champImages, threshHold)
+    items,_ = templateMatchList(gameImage, itemImages, threshHold)
+    starpower,_ = templateMatchList(gameImage, starImages, threshHold)
+    return champObject(champ[0], items, starpower[0])
+
+
+#def checkObjectAndDetect():
+
+
+def checkForPlanFlag():
+    print("HELLO")
+    return 
+
+def handleCarousel():
+    print("asjkldhf")
+
+def checkPixel():
+    print("asdf")
+#Figure out
+def fightTransition(gameState):
+    print("haksdjfh")
