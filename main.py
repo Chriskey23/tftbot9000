724c479bedde9380a1142db634cbc5168d0a8456
STKalinowski <kalinowski.stas@gmail.com>
2021-11-12 21:11:10 -0800

    Cleared out anything unnecessary

--- main.py
+++ main.py
@@ -19,27 +19,10 @@ def main():
     gameIsRunning = False
     #Game loop
     while(gameIsRunning):
-        #Check for phase and handle transition
+        #Grab screen
         screen = pyautogui.screenshot()
-        if gameState.phase is "carousel":
-            carouselCheck = ctr.tempateMatchList(screen.crop(), idSet.carouselPair, )
-            if not(ctr.templateMatchList):
-                #Empty list therefor not match therefor planning stage
-                gameState = ctr.handleCarousel(gameState, idSet)
-        elif gameState.phase is "planning":
-            res = ctr.checkPixel(idSet.planWarnColor, idSet.planWarnPos, screen)
-            if not(res) and gameState.planFlag:
-                gameState.phase = "setup"
-            elif res and not(gameState.planFlag):
-                gameState.planFlag = True   
-        elif gameState.phase is "setup":
-            if ctr.checkPixel(idSet.fightColor, idSet.fightColPos, screen):
-                gameState.phase = "fighting"
-        elif gameState.phase is "fighting":
-            if not(ctr.checkPixel(idSet.fightColor, idSet.fightColPos, screen)):
-                gameState = ctr.fightTransition(gameState, screen)
-        else:
-            raise Exception("Error: Not one of possible phases!")
+        #Screenstate funciton identifies the objects on the screen
+        screenState = getScreenState()
+        #Ai funciton handles the options
+        gameIsRunning = aiChoice(screenState)
 
-        #Give ai currentgame state and let it make decision
-        gameState = ai.makeNextDecision(gameState)
